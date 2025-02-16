4b776497c0c0704a831936981ddbc72d6db49763
latipda <lathifhamid8@gmail.com>
2025-02-17 03:09:58 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'I Have Something Since We Hardly See Each Other I Really Just Miss You AIDA'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
