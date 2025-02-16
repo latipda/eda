125992feddf7e50f199988627ae16c7384eb9791
latipda <lathifhamid8@gmail.com>
2025-02-17 03:17:47 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 10px;'><span/>`
   }
 }
 
